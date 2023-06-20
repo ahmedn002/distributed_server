ba67bb47f76451e2c3c80cbaad691eb3f2be5abd
Goru02 <ahmmednoaman5555@gmail.com>
2023-06-20 16:27:27 +0300

    updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,15 @@ const util = require('util');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
